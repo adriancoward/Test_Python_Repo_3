cb0a60f1353c622aee5f0b5b187313231cc568cb
adriancoward <adrian.v.coward@gmail.com>
2022-06-07 09:48:19 +0100

    Test

--- Test_Python_File_1.py
+++ Test_Python_File_1.py
@@ -2,3 +2,4 @@
 print("This is a test file for a new repository")
 print("Added a new line of code")
 print("Another line of code")
+print("Line added in main branch after deleting branch3 from VSC")
